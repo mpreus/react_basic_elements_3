0ef4f21c01231d30754a26f8fb273bb2c4b9395a
Maciej Preus <mpreus@onet.eu>
2019-11-28 16:22:26 +0100

    Add meta elements at html file and style modifications

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ class Message extends React.Component {
         return (
             <React.Fragment>
                 <h2 style={{textTransform:"uppercase", textAlign:"center"}}>Click the button to display the text</h2>
-                {this.state.messageIsActive ? <p>{text}</p> : null}
-                <button onClick={this.handleMessageButton} style={{backgroundColor:"yellow", border:"1px solid black", borderRadius:"4px", boxShadow:"3px 3px 3px grey", fontSize:"20px", padding:"10px"}}>
+                {this.state.messageIsActive ? <p style={{border:"1px solid grey", borderRadius:"4px", margin:"0 auto", padding:"8px", width:"800px"}}>{text}</p> : null}
+                <button onClick={this.handleMessageButton} style={{backgroundColor:"khaki", border:"1px solid black", borderRadius:"4px", boxShadow:"3px 3px 3px grey", fontSize:"20px", marginLeft:"45%", marginTop:"10px", padding:"10px"}}>
                     {this.state.messageIsActive ? 'Hide' : "Show"}
                 </button>
                 {/* one can also use: this.state.messageIsActive && <p>{text}</p> */}
